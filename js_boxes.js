d996a910f8df78325f0d8e967993cfe0e0bc68dc
GiePria <giedre.prialgauskiene@gmail.com>
2016-04-16 21:19:06 +0300

    Boxes
    10 dėžučių, css ir 2 js

---
+++ js_boxes.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $("#kv1").click(function(){
+        $(this).fadeOut();
+        $(this).fadeIn();
+      });
+});
+$(document).ready(function(){
+    $("#kv2").click(function(){
+        $(this).slideUp();
+        $(this).slideDown();
+     
+    });
+});
